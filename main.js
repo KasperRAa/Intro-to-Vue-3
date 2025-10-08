4c0713372b1614e25e1abfc9a0d628c668ad9608
kaa004-zealand <kaa004@edu.zealand.dk>
2025-10-08 11:32:06 +0200

    Done

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            blueSocksImage: 'assets/images/socks_blue.jpg',
+            greenSocksImage: 'assets/images/socks_green.jpg'
         }
     }
 })
